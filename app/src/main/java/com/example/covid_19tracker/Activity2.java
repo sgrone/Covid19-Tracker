0678e858fa5148ac088f68a9833647fa98fd42da
Sam Grone <sgrone@emich.edu>
2020-10-24 16:17:38 -0400

    added first 5 state image svg files

--- app/src/main/java/com/example/covid_19tracker/Activity2.java
+++ app/src/main/java/com/example/covid_19tracker/Activity2.java
@@ -3,11 +3,15 @@ package com.example.covid_19tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
+    private TextView stateName;
+    private ImageView stateImage;
     private String currentState;
 
     @Override
@@ -18,7 +22,38 @@ public class Activity2 extends AppCompatActivity {
         Intent intent = getIntent();
         currentState = intent.getStringExtra(MainActivity.EXTRA_TEXT);
 
-        TextView stateName = (TextView) findViewById(R.id.stateName);
+        stateName = (TextView) findViewById(R.id.stateName);
+        stateImage = findViewById(R.id.stateMap);
         stateName.setText(currentState);
+        setStateImage();
+
+
+    }
+
+    private void setStateImage() {
+        switch(currentState) {
+            case "Alabama":
+                stateImage.setImageResource(R.drawable.ic_alabama);
+                break;
+            case "Alaska":
+                stateImage.setImageResource(R.drawable.ic_alaska);
+                break;
+            case "Arizona":
+                stateImage.setImageResource(R.drawable.ic_arizona);
+                break;
+            case "Arkansas":
+                stateImage.setImageResource(R.drawable.ic_arkansas);
+                break;
+            case "California":
+                stateImage.setImageResource(R.drawable.ic_california);
+                break;
+            default:
+                break;
+        }
+        setStateColor();
+    }
+
+    private void setStateColor() {
+        stateImage.setColorFilter(Color.RED);
     }
 }
--- app/src/main/java/com/example/covid_19tracker/DataHandler.java
+++ app/src/main/java/com/example/covid_19tracker/DataHandler.java
@@ -1,19 +1,73 @@
 package com.example.covid_19tracker;
 
 import android.content.Context;
-import android.widget.Toast;
-
-import com.android.volley.Request;
+import android.util.Log;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
+import com.android.volley.toolbox.JsonArrayRequest;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.util.Vector;
 
+
+/*
+ * Updated by Payton to pull covid data from the covidtracking API and store it in several Vectors.
+ * */
 public class DataHandler {
+    //private String[] stateInitial = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
+    //"KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH",
+    //"OK", "OR", "PA", "RI", "SC","SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"};
+
+    //Any null values read in will be stored as "N/A" in the vectors.
+    // Stores a list of State initials pulled in from the covidtracker API in the order they appeared in the JSON file.
+    private Vector<String> stateID;
+    // Stores a list of the number of positive covid test results pulled in from the covidtracker API in the order they appeared in the JSON file.
+    private Vector<String> positiveResults;
+    // Stores a list of the number of deaths initials pulled in from the covidtracker API in the order they appeared in the JSON file.
+    private Vector<String> deaths;
+    // Stores a list of the number of people currently hospitalized pulled in from the covidtracker API in the order they appeared in the JSON file.
+    private Vector<String> hospitalizedCurrent;
+
+    //I'll add better comments later.
+    //These are for getting individual values out of the Vector lists.
+    public String getState(int index) {
+        return stateID.get(index);
+    }
+
+    public String getPosResult(int index) {
+        return positiveResults.get(index);
+    }
+
+    public String getDeathData(int index) {
+        return deaths.get(index);
+    }
+
+    public String getHospCur(int index) {
+        return hospitalizedCurrent.get(index);
+    }
+
+    //These are for getting the entire Vector lists.
+    public Vector<String> getStateList() {
+        return stateID;
+    }
+
+    public Vector<String> getPosResultList() {
+        return positiveResults;
+    }
+
+    public Vector<String> getDeathDataList() {
+        return deaths;
+    }
+
+    public Vector<String> getHospCurList() {
+        return hospitalizedCurrent;
+    }
+
+    private Context context;
+    RequestQueue requestQueue;
 
     public String getTvCases() {
         return tvCases;
@@ -33,46 +87,83 @@ public class DataHandler {
 
     private String tvCases = "Invalid";
     private String tvTotalDeaths = "Invalid";
-    private Context context;
 
     public DataHandler(Context context) {
         this.context = context;
+        requestQueue = Volley.newRequestQueue(context);
         fetchdata();
+        stateID = new Vector<String>();
+        positiveResults = new Vector<String>();
+        deaths = new Vector<String>();
+        hospitalizedCurrent = new Vector<String>();
     }
 
     private void fetchdata() {
+        Log.i("Check", "Fetch Data Method Check.");
         // Create a String request
         // using Volley Library
-        String url = "https://corona.lmao.ninja/v2/all";
+        String url = "https://api.covidtracking.com/v1/states/current.json";
 
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        //RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest request = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(String response) {
-                // Handle the JSON object and handle it inside try and catch
+            public void onResponse(JSONArray response) {
                 try {
-                    // Creating object of JSONObject
-                    JSONObject jsonObject = new JSONObject(response.toString());
-                    // Set the data in text view
-                    // which are available in JSON format
-                    // Note that the parameter inside
-                    // the getString() must match
-                    // with the name given in JSON format
-                    tvCases = jsonObject.getString("cases");
-                    tvTotalDeaths = jsonObject.getString("deaths");
+                    Log.i("Check", "Response Length: " + response.length());
+                    for (int x = 0; x < response.length(); x++) {
+                        JSONObject data = response.getJSONObject(x);
+                        String tempState = data.getString("state");
+
+                        /*The States of America Samoa(AS), Puerto Rico(PR),
+                         * District of Columbia(DC), Guam(GU), Northern Marianas(MP),
+                         * and the Virgin Islands(VI) are ignored here.
+                         * */
+                        if (!(tempState.equals("AS") || tempState.equals("PR") || tempState.equals("DC") ||
+                                tempState.equals("GU") || tempState.equals("MP") || tempState.equals("VI"))) {
+
+                            stateID.add(data.getString("state"));
+
+                            /*Some values pulled in from the JSON objects may be null if that data is
+                             * unavailable from a particular state. If this is the case, "N/A" is added
+                             * to the piece of data's respective Vector list in place of a value for that
+                             * state.*/
+                            if (data.getString("positive") != null) {
+                                positiveResults.add(data.getString("positive"));
+                            } else {
+                                positiveResults.add("N/A");
+                            }
+
+                            if (data.getString("death") != null) {
+                                deaths.add(data.getString("death"));
+                            } else {
+                                deaths.add("N/A");
+                            }
+
+                            if (data.getString("hospitalizedCurrently") != null) {
+                                hospitalizedCurrent.add(data.getString("hospitalizedCurrently"));
+                            } else {
+                                hospitalizedCurrent.add("N/A");
+                            }
+                            Log.i("Check", "State ID: " + data.getString("state"));
+                        }
+
+                        Log.i("Check", "State ID: " + data.getString("state"));
+                    }
+                    Log.i("Check", "For Loop Done,");
                 } catch (JSONException e) {
+                    Log.i("Check", "State Data Capture Failed!");
                     e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
+            // Handles errors that occur due to Volley
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context, error.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e("Volley", "Error");
             }
         });
 
-        RequestQueue requestQueue = Volley.newRequestQueue(context);
         requestQueue.add(request);
     }
-
-
 }
